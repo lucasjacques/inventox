12d18c48bbc3ed9ea46270f818fc20d64f3d866c
Lucas Jacques <lucas.jacs@gmail.com>
2025-06-02 16:54:13 -0300

    Add navbar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return(
     <div>
-      <Image src="/logo.svg" height={50} width={50} alt="Logo" />
-      <p className="text-xl font-semibold tracking-tight">Inventox</p>
+      
     </div>
   );
 }
