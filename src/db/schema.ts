2d0820ea0724a77d94c6a7306aeeab6e884a6a02
Lucas Jacques <lucas.jacs@gmail.com>
2025-06-21 17:30:15 -0300

    Add initial db schema for stockIns feature

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, uuid, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, timestamp, uniqueIndex, integer } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
@@ -9,3 +9,21 @@ export const users = pgTable("users", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 }, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
+
+export const groups = pgTable("groups", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: text("name").notNull(),
+});
+
+export const products = pgTable("products", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: text("name").notNull(),
+  groupsId: uuid("groups_id").references(() => groups.id),
+});
+
+export const stockIns = pgTable("stock_ins", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  value: integer("value").notNull(),
+  productsId: uuid("products_id").references(() => products.id),
+  authorsId: uuid("authors_id").references(()=> users.id)
+});
