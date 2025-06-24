8be7f5dbbbc92dfe2687003763857c87208bd01a
Lucas Jacques <lucas.jacs@gmail.com>
2025-06-24 14:23:21 -0300

    Add product and stock-ins relations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { pgTable, uuid, text, timestamp, uniqueIndex, integer } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
@@ -18,12 +19,30 @@ export const groups = pgTable("groups", {
 export const products = pgTable("products", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull(),
-  groupsId: uuid("groups_id").references(() => groups.id),
+  groupId: uuid("group_id").references(() => groups.id),
 });
+
+export const productRelations = relations(products, ({ one }) => ({
+  group: one(groups, {
+    fields: [products.groupId],
+    references: [groups.id],
+  }),
+}));
 
 export const stockIns = pgTable("stock_ins", {
   id: uuid("id").primaryKey().defaultRandom(),
   value: integer("value").notNull(),
-  productsId: uuid("products_id").references(() => products.id),
-  authorsId: uuid("authors_id").references(()=> users.id)
+  productId: uuid("product_id").references(() => products.id),
+  userId: uuid("user_id").references(()=> users.id)
 });
+
+export const stockInsRelations = relations(stockIns, ({ one }) => ({
+  product: one(products, {
+    fields: [stockIns.productId],
+    references: [products.id]
+  }),
+  user: one(users, {
+    fields: [stockIns.userId],
+    references: [users.id]
+  })
+}))
