3cf72418ef25040bd2d4913e9e4dda25d149710e
Lucas Jacques <lucas.jacs@gmail.com>
2025-07-07 15:16:02 -0300

    Add create procedure + add products insertion form

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -7,34 +7,34 @@ import { and, desc, eq, lt, or } from "drizzle-orm";
 
 export const productsRouter = createTRPCRouter({
   getMany: protectedProcedure
-  .input(z.object({
-    cursor: z.object({
-      id: z.string().uuid(),
-      name: z.string(),
-    })
-    .nullish(),
-    limit: z.number().min(1).max(100),
-  }))
-  .query(async ({ ctx, input }) => {
-    const { cursor, limit } = input;
+    .input(z.object({
+      cursor: z.object({
+        id: z.string().uuid(),
+        name: z.string(),
+      })
+      .nullish(),
+      limit: z.number().min(1).max(100),
+    }))
+    .query(async ({ ctx, input }) => {
+      const { cursor, limit } = input;
 
-    const data = await db
-      .select()
-      .from(products)
-      .where(and(
-        cursor
-        ? or (
-          lt(products.name, cursor.name),
-          and(
-            eq(products.name, cursor.name),
-            lt(products.id, cursor.id),
+      const data = await db
+        .select()
+        .from(products)
+        .where(and(
+          cursor
+          ? or (
+            lt(products.name, cursor.name),
+            and(
+              eq(products.name, cursor.name),
+              lt(products.id, cursor.id),
+            )
           )
-        )
-        : undefined
-      ))
-      .orderBy(desc(products.name), desc(products.id))
-      .limit(limit + 1)
-      .innerJoin(groups, eq(products.groupId, groups.id));
+          : undefined
+        ))
+        .orderBy(desc(products.name), desc(products.id))
+        .limit(limit + 1)
+        .innerJoin(groups, eq(products.groupId, groups.id));
 
       const hasMore = data.length > limit;
       // Remove the last item if there is more that
@@ -48,9 +48,33 @@ export const productsRouter = createTRPCRouter({
         }
         : null
 
+      const groupsData = await db
+        .select()
+        .from(groups)
+        .orderBy(groups.name)
+
       return {
         items,
+        groupsData,
         nextCursor,
       };
-  })
+    }),
+  create: protectedProcedure
+    .input(z.object({
+      name: z.string(),
+      groupId: z.string().uuid(),
+    }))
+    .mutation(async ({ input }) => {
+      const [ product ] = await db
+        .insert(products)
+        .values({
+          name: input.name,
+          groupId: input.groupId,
+        })
+        .returning();
+      
+      return {
+        product: product,
+      };
+    })
 })
